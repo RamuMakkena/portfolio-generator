a7d4bccebf3bfb1d5773cfbff07dc327fbfa8cd3
Ramu Makkena <MRamuMailer@gmail.com>
2022-04-10 13:29:43 -0500

    Adding a webpage

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
+const fs = require('fs');
 const profileDataArgs = process.argv.slice(2,process.argv.length);
+const [name,github] =profileDataArgs;
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-      });
-}
-
-printProfileData(profileDataArgs);
-
+fs.writeFile('index.html', generatePage(name,github), err => {
+    if(err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
